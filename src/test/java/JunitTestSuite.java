5800c90600e54237f9e6edb261b39ebb5156ef9d
Dharel <marcin.oczkowicz@interia.eu>
2020-06-21 18:14:38 +0200

    Test Suite

--- src/test/java/JunitTestSuite.java
+++ src/test/java/JunitTestSuite.java
@@ -1,11 +1,6 @@
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-@RunWith(Suite.class)
-
-@Suite.SuiteClasses({
-        Algorithm_test.class ,Algorithm2_test.class
-})
 
 public class JunitTestSuite {
 }
